1edc26c7d2c73e29e9c84486281ac46082e9c45d
SARezaHashemi <samhashemi87@gmail.com>
2024-08-10 20:25:48 +0330

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HonorOfEFCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
